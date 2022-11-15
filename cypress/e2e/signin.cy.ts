9868192119c2a08f276adc744f11bd20678bcf01
bcaton <bcaton@redhat.com>
2022-11-15 11:46:57 -0500

    Adding e2e tests

--- cypress/e2e/signin.cy.ts
+++ cypress/e2e/signin.cy.ts
@@ -3,7 +3,7 @@
 describe('Signin page', () => {
   it('Succesful signin', () => {
     cy.visit(`/signin`);
-    cy.get('#pf-login-username-id').type('username');
+    cy.get('#pf-login-username-id').type('user1');
     cy.get('#pf-login-password-id').type('password');
     cy.get('button[type=submit]').click();
     cy.url().should('include', '/organizations');
