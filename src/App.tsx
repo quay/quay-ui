d9b3e63476c41fdbcf80ff63a8ca1f5c51b0302c
Brandon Caton <bcaton@redhat.com>
2022-12-08 10:11:58 -0500

    Adding permissions management to repository settings (#150)
    Allows for management of user and robot permissions at the repository settings level.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {Suspense, useEffect} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 import 'src/App.css';
 
@@ -7,15 +8,25 @@ import {LoadingPage} from 'src/components/LoadingPage';
 import {StandaloneMain} from 'src/routes/StandaloneMain';
 import {Signin} from 'src/routes/Signin/Signin';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Suspense fallback={<LoadingPage />}>
-          <Routes>
-            <Route path="/*" element={<StandaloneMain />} />
-            <Route path="/signin" element={<Signin />} />
-          </Routes>
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route path="/*" element={<StandaloneMain />} />
+              <Route path="/signin" element={<Signin />} />
+            </Routes>
+          </QueryClientProvider>
         </Suspense>
       </BrowserRouter>
     </div>
